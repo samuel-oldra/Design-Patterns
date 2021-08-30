278dffe135db178341d01452ef72dd249d52b82c
Samuel B. Oldra <samuel.oldra@gmail.com>
2021-08-30 13:42:04 -0300

    Adicionada classe Client para testar MobileSniffer

---
+++ POO/Client.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once 'MobileSniffer.php';
+
+class Client
+{
+    private $mobSniff;
+
+    public function __construct()
+    {
+        $this->mobSniff = new MobileSniffer();
+        print 'Device = ' . $this->mobSniff->findDevice() . '<br/>';
+        print 'Browser = ' . $this->mobSniff->findBrowser() . '<br/>';
+    }
+}
+
+$trigger = new Client();
+
+?>
