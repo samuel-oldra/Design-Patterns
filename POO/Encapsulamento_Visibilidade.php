737bb7fd9cc8e4c267550761c91af910f925750e
Samuel B. Oldra <samuel.oldra@gmail.com>
2023-09-13 00:00:00 -0300

    Encapsulamento e Visibilidade (public)

--- POO/Encapsulamento_Visibilidade.php
+++ POO/Encapsulamento_Visibilidade.php
@@ -50,6 +50,33 @@ class ConcreteProtect extends ProtectVis
     }
 }
 
-$worker = new ConcreteProtect();
+//$worker = new ConcreteProtect();
+
+// PUBLIC
+
+class PublicVis
+{
+    private $password;
+
+    private function openSesame($someData)
+    {
+        $this->password = $someData;
+
+        if ($this->password == "secret") {
+            echo "You're in!<br/>";
+        } else {
+            echo "Release the hounds!<br/>";
+        }
+    }
+
+    public function unlock($safe)
+    {
+        $this->openSesame($safe);
+    }
+}
+
+$worker = new PublicVis();
+$worker->unlock("secret");
+$worker->unlock("duh");
 
 ?>
